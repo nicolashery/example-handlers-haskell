46ee10d54a747cd530c0a619d3784e73889d9cef
Nicolas Hery <hery.nicolas@gmail.com>
2022-09-07 17:23:13 +0200

    Add logging to Servant example

--- src/App/Servant.hs
+++ src/App/Servant.hs
@@ -2,38 +2,70 @@ module App.Servant (main) where
 
 import App.Config (Config (configPaymentMaxRetries), configInit)
 import App.Text (tshow)
+import Blammo.Logging
+  ( HasLogger (loggerL),
+    Logger,
+    LoggingT,
+    Message ((:#)),
+    MonadLogger (monadLoggerLog),
+    logInfo,
+    runLoggerLoggingT,
+    (.=),
+  )
+import Blammo.Logging.Simple (newLoggerEnv)
 import Control.Monad.Reader (MonadReader, ReaderT, asks, runReaderT)
+import Control.Monad.Trans (lift)
 import Data.Text (Text)
+import Lens.Micro (lens)
 import Network.Wai.Handler.Warp (run)
-import Servant (Capture, Handler, HasServer (ServerT), PlainText, Post, Proxy (Proxy), hoistServer, serve, (:>))
+import Servant
+  ( Capture,
+    Handler,
+    HasServer (ServerT),
+    PlainText,
+    Post,
+    Proxy (Proxy),
+    hoistServer,
+    serve,
+    (:>),
+  )
 
 data App = App
   { appConfig :: Config,
-    appLogger :: ()
+    appLogger :: Logger
   }
+
+instance HasLogger App where
+  loggerL = lens appLogger $ \x y -> x {appLogger = y}
 
 appInit :: IO App
 appInit = do
   config <- configInit
+  logger <- newLoggerEnv
   let app =
         App
           { appConfig = config,
-            appLogger = ()
+            appLogger = logger
           }
   pure app
 
 newtype AppM a = AppM
-  { unAppM :: ReaderT App Handler a
+  { unAppM :: ReaderT App (LoggingT Handler) a
   }
   deriving (Functor, Applicative, Monad, MonadReader App)
 
+instance MonadLogger AppM where
+  monadLoggerLog loc logSource logLevel msg =
+    AppM $ lift $ monadLoggerLog loc logSource logLevel msg
+
 appToHandler :: App -> AppM a -> Handler a
-appToHandler app m = runReaderT (unAppM m) app
+appToHandler app m = runLoggerLoggingT app $ runReaderT (unAppM m) app
 
 type Api = "cart" :> Capture "cartId" Text :> "purchase" :> Post '[PlainText] Text
 
 postCartPurchaseHandler :: Text -> AppM Text
 postCartPurchaseHandler cartId = do
+  logInfo $ "Cart purchase starting" :# ["cart_id" .= cartId]
   paymentMaxRetries <- asks (configPaymentMaxRetries . appConfig)
   let response =
         mconcat
@@ -44,6 +76,7 @@ postCartPurchaseHandler cartId = do
             tshow paymentMaxRetries,
             "\n"
           ]
+  logInfo $ "Cart purchase successful" :# ["cart_id" .= cartId]
   pure response
 
 server :: ServerT Api AppM
