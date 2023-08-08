6809a4d033849c0efc2d44a194dc01b100cbf914
Nicolas Hery <hery.nicolas@gmail.com>
2023-08-08 16:01:23 -0400

    Upgrade dependencies

--- src/App/Db.hs
+++ src/App/Db.hs
@@ -7,12 +7,7 @@ where
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (MonadReader, asks)
-import Data.Pool
-  ( Pool,
-    PoolConfig (PoolConfig, createResource, freeResource, poolCacheTTL, poolMaxResources),
-    newPool,
-    withResource,
-  )
+import Data.Pool (Pool, defaultPoolConfig, newPool, withResource)
 import Data.String.Conversions (cs)
 import Data.Text (Text)
 import Database.PostgreSQL.Simple (Connection, close, connectPostgreSQL)
@@ -22,15 +17,18 @@ class HasDbPool env where
   getDbPool :: env -> Pool Connection
 
 dbInit :: Text -> IO (Pool Connection)
-dbInit databaseUrl =
-  let poolConfig =
-        PoolConfig
-          { createResource = connectPostgreSQL (cs databaseUrl),
-            freeResource = close,
-            poolCacheTTL = 10,
-            poolMaxResources = 10
-          }
-   in newPool poolConfig
+dbInit databaseUrl = do
+  newPool $
+    defaultPoolConfig
+      create
+      destroy
+      poolTtl
+      poolSize
+  where
+    create = connectPostgreSQL (cs databaseUrl)
+    destroy = close
+    poolTtl = 10
+    poolSize = 10
 
 withConn ::
   (MonadReader env m, HasDbPool env, MonadUnliftIO m) => (Connection -> IO a) -> m a
